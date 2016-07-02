e4ccd5bb475f475e2f94dd7192ca19eaf1588c6c
Michael Busta <busta2356@gmail.com>
2016-07-02 11:51:59 -0400

    added bg img/button spoiler/jquery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-//hide warning
-$(".warning").hide();
-//show warning slow
-$(".warning").show("slow")
+//prevent spoilerphobes from seeing spoilers
+//solution: hide spoilers and reveal them through user interaction
+
+//1. hide spoiler
+$(".spoiler span").hide();
+
+//2. add a button
+$(".spoiler").append("<button>Reveal Spoiler!</button>")
+
+//3. when button is pressed
+$("button").click(function() {
+  //show spoiler
+  $(".spoiler span").show();
+  //hide button
+  $(this).remove();
+});
